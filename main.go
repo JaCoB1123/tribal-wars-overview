70e4c948fdf1ae64026b758cd816a1558237e47f
Jan Bader <jan.bader@ub-software.de>
2018-12-23 20:40:30 +0100

    Remove debugging

--- main.go
+++ main.go
@@ -53,7 +53,6 @@ func (l *myListener) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Fprintln(w, "{\"status\":\"success\"}")
 	} else if r.Method == "GET" {
-		fmt.Println(r.URL.Path)
 		if r.URL.Path == "/data" {
 			if l.current == nil || len(l.current) == 0 {
 				fmt.Fprint(w, "{}")
