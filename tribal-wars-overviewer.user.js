978da54d968af09a38047072fc25a9faa9cd206c
Jan Bader <jan.bader@ub-software.de>
2018-12-25 21:47:32 +0100

    Also parse trades

--- tribal-wars-overviewer.user.js
+++ tribal-wars-overviewer.user.js
@@ -62,6 +62,45 @@
         villages[id]=village;
     }
 
+    function getId(text) {
+        let idMatch = /id=\d+/;
+        let matches = text.match(idMatch);
+        if(matches == null) {
+            return null;
+        }
+
+        return +matches[0].substring(3);
+    }
+
+    function parseTrades(villages, element) {
+        let cells = $(element).find("td");
+        let ressources = $(cells[8]).find(".nowrap");
+        let info = {
+            sender: getId(cells[1].innerHTML),
+            senderVillage: getId(cells[2].innerHTML),
+            receiver: getId(cells[3].innerHTML),
+            receiverVillage: getId(cells[4].innerHTML),
+            eda: cells[6].innerText.split(":").map((x, i) => (+x) * [60*60, 60, 1][i]).reduce((sum, c) => sum+c, 0),
+            iron: 0,
+            stone: 0,
+            wood: 0,
+        };
+
+        ressources.each(function(i, r) {
+            if(r.firstChild.classList.contains("wood")){
+                info.wood = +r.innerText.replace('.', '');
+            }
+            if(r.firstChild.classList.contains("stone")){
+                info.stone = +r.innerText.replace('.', '');
+            }
+            if(r.firstChild.classList.contains("iron")){
+                info.iron = +r.innerText.replace('.', '');
+            }
+        });
+
+        console.log(info);
+    }
+
     GM.getValue("die_staemme", {}).then((data) => {
         var villages = data.villages || {};
         $("#production_table tr").each(function(i, e) {
@@ -79,9 +118,16 @@
             parseBuildings(villages, e);
         });
 
+        $("#trades_table tr").each(function(i, e) {
+            // HEADER
+            if(i == 0) return;
+
+            parseTrades(villages, e);
+        });
+
         try {
-        let totals= {ressources: {wood:0, stone: 0, iron: 0}, units: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
-        for(let id in villages) {
+            let totals= {ressources: {wood:0, stone: 0, iron: 0}, units: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+            for(let id in villages) {
             let village = villages[id];
             totals.ressources.wood += village.ressources.wood;
             totals.ressources.stone += village.ressources.stone;
